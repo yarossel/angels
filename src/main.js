495c8cd0d0f0cd5b737fbddd1adeeb282b1c1719
unknown <lyywnlraw@gmail.com>
2025-06-01 00:46:29 +0300

    третий день - доделала анимации (потом вернусь, чтобы сделать еще одну), сделала страницу с билетами

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 const buttons = document.querySelectorAll('[data-carousel-btn]');
+const masks = document.querySelector('[data-slider]').querySelectorAll('.character-mask');
 
 buttons.forEach(button => {
   button.addEventListener('click', () => {
@@ -6,14 +7,47 @@ buttons.forEach(button => {
     const slides = button
     .closest('[data-carousel]')
     .querySelector('[data-slides]');
+    const slider = button.closest('[data-carousel]').querySelector('[data-slider]');
 
     const activeSlide = slides.querySelector('[data-active]');
+    const activeMask = slider.querySelector('[data-active]');
     let newIndex = [...slides.children].indexOf(activeSlide) + offset;
     if (newIndex < 0) newIndex = slides.children.length - 1;
     if (newIndex >= slides.children.length) newIndex = 0;
+    delete activeSlide.dataset.active;
+    delete activeMask.dataset.active;
 
     slides.children[newIndex].dataset.active = true;
-    delete activeSlide.dataset.active
+    masks[newIndex].dataset.active = true;
+
+    activeBorder.style.left = `${masks[newIndex].offsetLeft}px`; 
   })
 })
+
+const activeBorder = document.querySelector('.active-border');
+
+masks.forEach((mask, index) => {
+  mask.addEventListener('click', function() {
+    const slides = mask.closest('[data-carousel').querySelector('[data-slides]');
+    const slider = document.querySelector('[data-slider');
+
+    const relativeLeft = this.offsetLeft;
+    activeBorder.style.left = `${relativeLeft}px`;
+
+    const activeSlide = slides.querySelector('[data-active]');
+    const activeMask = slider.querySelector('[data-active]');
+    delete activeMask.dataset.active;
+    delete activeSlide.dataset.active;
+    mask.dataset.active = true;
+    slides.children[index].dataset.active = true;
+  })
+})
+
+window.addEventListener('DOMContentLoaded', () => { 
+  const activeMask = document.querySelector('.character-mask[data-active]');
+
+  if (activeMask) { 
+    activeBorder.style.left = `${activeMask.offsetLeft}px`; 
+  } 
+});
 
